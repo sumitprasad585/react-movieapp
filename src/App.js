5058ccf8b7340a1d09bff4c3944f4b895dda0d3a
Sumit Prasad <sumitprasad585@gmail.com>
2022-12-26 15:44:39 +0530

    same component on multiple routes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/ui/Navbar";
 import Movies from "./components/Movies";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
         <Switch>
-          <Route exact
-            path="/now_playing"
+          <Route exact path="/">
+            <Redirect to="now_playing" />
+          </Route>
+          <Route
+            key="now-playing-movies"
+            exact path="/now_playing"
             render={(routeProps) => <Movies {...routeProps} />}
           />
           <Route
+            key="upcoming-movies"
             exact path="/upcoming"
             render={(routeProps) => <Movies {...routeProps} />}
           />
           <Route
+            key="popular-movies"
+            exact path="/popular"
+            render={(routeProps) => <Movies {...routeProps} />}
+          />
+          <Route
+            key="top-rated-movies"
             exact path="/top_rated"
             render={(routeProps) => <Movies {...routeProps} />}
           />
